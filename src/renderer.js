38b590686505b619a1df125b6118dbeb2d15b81b
Taiki Komoda <komoda@jins.com>
2024-06-13 16:10:00 +0900

    still testing

---
+++ src/renderer.js
@@ -0,0 +1,48 @@
+import './index.css';
+
+window.onload = function () {
+    function recognize() {
+      let speechRecognition = webkitSpeechRecognition || SpeechRecognition;
+      let recognition = null;
+      let e = document.getElementById("result_text");
+      let currentHTML = ''
+  
+      function init(){
+        recognition = new speechRecognition();
+        recognition.lang = 'ja-JP';
+        recognition.continuous = false;
+        recognition.interimResults = true;
+  
+        recognition.onresult = (event) => {
+          let text = event.results[0][0].transcript;
+          e.innerHTML = currentHTML + text;
+  
+          if (event.results[0].isFinal){
+            restart();
+          }
+        }
+  
+        recognition.onspeechstart = (event) => {
+          currentHTML = ''
+        }
+  
+        recognition.onerror = (event) => {
+          restart();
+        }
+  
+        recognition.onsoundend = (event) => {
+          restart();
+        }
+      }
+  
+      function restart(){
+        recognition.stop();
+        init();
+        recognition.start();
+      }
+  
+      init();
+      recognition.start();
+    }
+    recognize();
+  }
