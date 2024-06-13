4799e5dc3f63f1c0c12d57e865e158c414790105
Taiki Komoda <komoda@jins.com>
2024-06-13 16:13:37 +0900

    wip

--- src/renderer.js
+++ src/renderer.js
@@ -2,18 +2,20 @@ import './index.css';
 
 window.onload = function () {
     function recognize() {
-      let speechRecognition = webkitSpeechRecognition || SpeechRecognition;
+      let speechRecognition = webkitSpeechRecognition;
       let recognition = null;
       let e = document.getElementById("result_text");
       let currentHTML = ''
   
       function init(){
+        console.log("starting")
         recognition = new speechRecognition();
         recognition.lang = 'ja-JP';
         recognition.continuous = false;
         recognition.interimResults = true;
   
         recognition.onresult = (event) => {
+          console.log("event")
           let text = event.results[0][0].transcript;
           e.innerHTML = currentHTML + text;
   
